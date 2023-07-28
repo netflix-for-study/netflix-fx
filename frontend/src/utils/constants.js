ced1bfe2c49461a06b91ed12cc77c7b5f780f42b
shyeon0602 <july8560@naver.com>
2023-07-28 16:12:21 +0900

    수정: api명 수정

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,9 +3,9 @@ export const port = 8080;
 export const ip = "localhost";
 
 export const API_PATHS = {
-  USERS: "/api/users",
-  AUTH: "/api/auth",
-  MOVIES: "/api/movies",
-  PROFILE: "/api/profile",
-  OAUTH: "/api/oauth",
+  USERS: "/users",
+  AUTH: "/auth",
+  MOVIES: "/movies",
+  PROFILE: "/profile",
+  OAUTH: "/oauth",
 };
