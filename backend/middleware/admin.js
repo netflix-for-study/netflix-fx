2287e9188c241401b9c10bb0132ac9f1d581e016
shyeon0602 <july8560@naver.com>
2023-08-07 00:51:51 +0900

    수정: admin 속성 설정

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,5 +1,8 @@
 module.exports = function (req, res, next) {
+  //username이 admin인 경우, 관리자로 설정
+  const isAdmin = req.user.username === "admin";
+  //관리자 확인
+  if (isAdmin) return next();
   //관리자 외 접근거부(관리자 권한 확인)
-  if (!req.user.isAdmin) return res.status(403).send("관리자 외 접근 거부");
-  next();
+  if (!req.user.isAdmin) return res.status(403).send("관리자 권한이 없습니다.");
 };
